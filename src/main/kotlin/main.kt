68d85d5a5a2e83c1833f3ce5c7ebe2f04d93ecb6
bgchoi205 <choibg205@naver.com>
2021-04-16 11:19:49 +0900

    overrideFunToString

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,35 @@
 fun main() {
 
-    for(dan1 in 1..9){
-        for(dan2 in 1..9){
-            for(i in 1..9){
-                println("$dan1 * $dan2 * $i = ${dan1 * dan2 * i}")
-            }
-        }
+//    for(dan1 in 1..9){
+//        for(dan2 in 1..9){
+//            for(i in 1..9){
+//                println("$dan1 * $dan2 * $i = ${dan1 * dan2 * i}")
+//            }
+//        }
+//    }
+
+
+    val human1 = Human();
+    human1.id = 1;
+    human1.name = "철수";
+    human1.sex = "male";
+    human1.age = 29;
+    human1.home = "철원";
+    println(human1)
+
+
+
+}
+
+
+class Human {
+    var id: Int = 0;
+    var name: String = "";
+    var sex: String = "";
+    var age: Int = 0;
+    var home: String = "";
+
+    override fun toString() : String {
+        return "id : $id, name : $name, sex : $sex, age : $age, home : $home"
     }
 }
