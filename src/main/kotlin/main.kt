ed0c57dd06e10525c47a8c25623dde91a741970b
bgchoi205 <choibg205@naver.com>
2021-04-16 09:59:49 +0900

    999단

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    print("hihi")
+
+    for(dan1 in 1..9){
+        for(dan2 in 1..9){
+            for(i in 1..9){
+                println("$dan1 * $dan2 * $i = ${dan1 * dan2 * i}")
+            }
+        }
+    }
 }
