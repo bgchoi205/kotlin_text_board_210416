58f7b6450318b0cb2a3d06d76e78a9ea9f88c7ad
bgchoi205 <choibg205@naver.com>
2021-04-16 12:23:18 +0900

    생성자

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,27 +9,61 @@ fun main() {
 //    }
 
 
-    val human1 = Human();
-    human1.id = 1;
-    human1.name = "철수";
-    human1.sex = "male";
-    human1.age = 29;
-    human1.home = "철원";
-    println(human1)
+    val human1 = Human(1,"철수", "male", 29, "철원");
+
+    println(human1);
+
+    val human2 = Human(2,"영희", "female", 30, "남원" );
+
+    println(human2);
+
+
+    val human3 = Human(3, "영수", "male", 31, "진천");
+
+    println(human3);
 
 
 
 }
 
+// 일반 클래스
+//class Human {
+//    var id: Int = 0;
+//    var name: String = "";
+//    var sex: String = "";
+//    var age: Int = 0;
+//    var home: String = "";
+//
+//    // 인라인 메서드 - 한 줄일 떄 가능
+//    fun introduceMsg() : String = "안녕하세요. 저는 $age 살 $name 입니다. 고향은 $home 입니다.";
+//
+//
+//    override fun toString() : String = "id : $id, name : $name, sex : $sex, age : $age, home : $home"
+//}
 
-class Human {
-    var id: Int = 0;
-    var name: String = "";
-    var sex: String = "";
-    var age: Int = 0;
-    var home: String = "";
 
-    override fun toString() : String {
-        return "id : $id, name : $name, sex : $sex, age : $age, home : $home"
-    }
+// 주생성자
+//class Human (var id: Int,
+//             var name: String,
+//             var sex: String,
+//             var age: Int,
+//             var home: String){
+//
+//
+//    // 인라인 메서드 - 한 줄일 떄 가능
+//    fun introduceMsg() : String = "안녕하세요. 저는 $age 살 $name 입니다. 고향은 $home 입니다.";
+//
+//
+//    override fun toString() : String = "id : $id, name : $name, sex : $sex, age : $age, home : $home"
+//}
+
+//데이터생성자
+data class Human (var id: Int,
+             var name: String,
+             var sex: String,
+             var age: Int,
+             var home: String){
+
+
+
 }
